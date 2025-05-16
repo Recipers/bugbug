48ffc33d4633f0a8ac5b3944c1a4985ea452f216
Rohsooil <food8123@gmail.com>
2025-05-16 21:45:17 +0900

    Initial Commit

---
+++ src/main/java/com/recipers/bugbug/BugbugApplication.java
@@ -0,0 +1,13 @@
+package com.recipers.bugbug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugbugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugbugApplication.class, args);
+	}
+
+}
